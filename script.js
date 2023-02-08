b71ed5e694f768564388662f9859b508e39eeab2
Matheus Rodrigues de Mello <matheus_usagui@hotmail.com>
2023-02-08 16:26:31 -0300

    Instanceof

--- script.js
+++ script.js
@@ -186,3 +186,200 @@
 // `;
 //   });
 // }
+// async function fetchCursos() {
+//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
+//   const data = await response.json();
+//   mostrarCursos(data);
+// }
+// fetchCursos();
+// interface Cursos {
+//   nome: string;
+//   gratuito: string;
+//   horas: number;
+//   idAulas: number[];
+//   nivel: 'iniciante' | 'avancado';
+//   aulas: number;
+//   tags: string[];
+// }
+// function mostrarCursos(cursos: Array<Cursos>) {
+//   cursos.forEach((curso) => {
+//     let color;
+//     curso.nivel === 'iniciante' ? (color = 'blue') : (color = 'red');
+//     document.body.innerHTML += `
+//   <div style='backGround: #eee; padding: 0rem 1.2rem'>
+//   <h1 style='color: ${color}'>Curso: ${curso.nome}<h1/>
+//   <p>Horas: ${curso.horas}<p/>
+//   <p>Aulas: ${curso.idAulas.join(' | ')}<p/>
+//   <p>Nível: ${curso.nivel}<p/>
+//   <p>${curso.aulas} Aulas<p/>
+//   <p>Grade: ${curso.tags.join(', ')}<p/>
+//   <div/>
+//   `;
+//   });
+// }
+// interface Curso {
+//   nome: string;
+//   horas: number;
+// }
+// function mostrarCursos(cursos: Curso[]) {
+//   cursos.forEach((curso) => {
+//     document.body.innerHTML += `
+//       <div>
+//         <h2>${curso.nome}</h2>
+//         <p>Horas: ${curso.horas}</p>
+//       </div>
+//     `;
+//   });
+// }
+// const dados: any = 'o any gera problemas';
+// mostrarCursos(dados);
+// const numbers = [
+//   10.0, 8.7, 10.0, 10.0, 9.6, 9.9, 9.2, 10.0, 7.9, 7.3, 8.5, 10.0, 10.0, 7.4,
+//   8.3, 9.2, 10.0, 10.0, 10.0, 10.0, 7.9, 9.1, 9.9, 8.0, 10.0, 8.1, 10.0, 9.3,
+//   8.2, 6.0, 9.0, 10.0, 10.0, 8.3, 9.4, 10.0, 10.0, 9.0, 10.0, 7.5, 10.0, 10.0,
+//   10.0, 10.0, 9.0, 10.0, 10.0, 9.1, 7.8, 10.0, 10.0, 9.7, 10.0, 10.0, 10.0,
+//   10.0, 8.6, 9.9, 10.0,
+// ];
+// function media(array: Array<number>) {
+//   return +array
+//     .reduce((acc, value) => {
+//       return acc + value / numbers.length;
+//     }, 0)
+//     .toFixed(2);
+// }
+// console.log(media(numbers));
+// const funcional = (array: Array<number>) => () => {
+//   return +array
+//     .reduce((acc, value) => {
+//       return acc + value / array.length;
+//     }, 0)
+//     .toFixed(2);
+// };
+// console.log(funcional(numbers)())
+// interface Product {
+//   nome?: string
+//   ano?: number
+// }
+// const jogo: Product = {
+//   nome: 'Ragnarok',
+//   ano: 2005
+// }
+// console.log(jogo)
+// class Produto {
+//   nome?: string;
+//   preco?: number;
+//   constructor(nome?: string, preco?: number) {
+//     this.nome = nome;
+//     this.preco = preco;
+//   }
+//   precoEmReal() {
+//     return `R$ ${this.preco}`;
+//   }
+// }
+// const livro = new Produto('Game of Thrones', 200);
+// class Livro {
+//   autor?: string;
+//   constructor(autor?: string) {
+//     this.autor = autor;
+//   }
+// }
+// class Jogo {
+//   jogadores?: number;
+//   constructor(jogadores?: number) {
+//     this.jogadores = jogadores;
+//   }
+//   playersNumber() {
+//     return `Jogo para apenas ${this.jogadores} jogadores`;
+//   }
+// }
+// function buscarProduto(busca: string) {
+//   if (busca === 'O Hobbit') return new Livro('J. J. R. Tokken');
+//   if (busca === 'Call of Duty') return new Jogo(2);
+//   if (busca === 'DarkSouls') return new Jogo(1).playersNumber();
+//   else return null;
+// }
+// function buscarNovoProduto(busca: string) {
+//   switch (busca) {
+//     case 'O Hobbit':
+//       return new Livro('J. J. R. Tokken');
+//       break;
+//     case 'Call of Duty':
+//       return new Jogo(2);
+//       break;
+//     case 'DarkSouls':
+//       return new Jogo(1).playersNumber();
+//       break;
+//     default:
+//       return null;
+//   }
+// }
+// const produto = buscarNovoProduto('O Hobbit');
+// console.log(produto);
+// if (produto instanceof Livro) {
+//   console.log(produto.autor);
+// }
+// const produto = buscarProduto('O Hobbit');
+// const produto2 = buscarProduto('Call of Duty');
+// const produto3 = buscarProduto('DarkSouls');
+// if (produto instanceof Livro) {
+//   console.log(produto.autor);
+// }
+// if (produto2 instanceof Jogo) {
+//   console.log(produto2.jogadores);
+//   console.log(produto2.playersNumber());
+// }
+// if (produto3 instanceof Jogo) {
+//   console.log(produto3.jogadores);
+// }
+// class Produto {
+//   nome: string;
+//   constructor(nome: string) {
+//     this.nome = nome;
+//   }
+// }
+// class Livro extends Produto {
+//   autor: string;
+//   constructor(nome: string, autor: string) {
+//     super(nome);
+//     this.autor = autor;
+//   }
+// }
+// class Jogo extends Produto {
+//   jogadores: number;
+//   constructor(nome: string, jogadores: number) {
+//     super(nome);
+//     this.jogadores = jogadores;
+//   }
+//   showParms() {
+//     return `Este jogo permite ${this.jogadores} jogadores`;
+//   }
+// }
+// const buscarProduto = (nome: string) => {
+//   switch (nome) {
+//     case 'DarkSouls':
+//       return new Jogo('DarkSouls', 1);
+//       break;
+//     case 'Game of Thrones':
+//       return new Livro('Game of Thrones', 'George R.R. Martin');
+//       break;
+//     default:
+//       return null;
+//   }
+// };
+// const produto = buscarProduto('DarkSouls');
+// const produto2 = buscarProduto('Game of Thrones');
+// if (produto instanceof Produto) console.log(produto);
+// if (produto instanceof Jogo) console.log(produto.showParms());
+// if(produto2 instanceof Produto) console.log(produto2?.nome)
+// if(produto2 instanceof Livro) console.log(produto2)
+// const link = document.getElementById('origamid');
+// if (link instanceof HTMLAnchorElement)
+//   link.href = link.href.replace('http//', 'https//');
+// const links = document.querySelectorAll('.link');
+// links.forEach((link) => {
+//   if (link instanceof HTMLElement) changeElementStyle(link, '#fb5');
+// });
+// function changeElementStyle(element: HTMLElement, color: string) {
+//   element.style.color = color;
+//   element.style.border = `2px solid ${color}`;
+// }
